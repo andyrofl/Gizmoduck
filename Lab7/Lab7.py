a208760376a90353de1796418e776d3e8c66d905
andy <andy@andyrofl.com>
2022-10-28 19:48:07 -0400

    initial commit

--- Lab7/Lab7.py
+++ Lab7/Lab7.py
@@ -11,20 +11,20 @@ from pybricks.robotics import DriveBase
 left = Motor(Port.A)
 right = Motor(Port.B)
 robot_drive = DriveBase(left, right, 41, 102) #outisdes: 124 insides: 80 avg 102
-straight_controller = TouchSensor(?))
-go_right_controller = UltrasonicSensor(?)
+straight_controller = TouchSensor(?)
+go_right_controller = UltrasonicSensor(?) #ports, gotta figure out where each sensor goes first
 go_left_controller = LightSensor(?)
 
 # Variables and Constants
-robot_drive.settings(?, ?, ?, ?)
+robot_drive.settings(?, ?, ?, ?) #speed mm/s, acceleration mm/s^2, turn rate deg/s, turn acceleration deg/s^2
 
 # Main Code
 for count in range(25):
-    if straight_controller.pressed() == True:
-        robot_drive.straight(-25.4) #back up an inch
-    elif go_left_controller.ambient() < 35: #35 is ambient and hitting something
-        robot_drive.turn(?)
-    elif go_right_controller.distance() < ?:
-        robot_drive.turn(?)
+    if straight_controller.pressed() == True: #0-1 true-false
+        robot_drive.straight() #back up an inch
+    elif go_left_controller.ambient() < 20: #35 is ambient and hitting something
+        robot_drive.turn(?) #left like 45 degrees
+    elif go_right_controller.distance() < 40: #0-175ish, 255 is either really far away or inside of it.
+        robot_drive.turn(?) #right like 45
     else:
-        wait(?)
+        wait(?) #how long wait
